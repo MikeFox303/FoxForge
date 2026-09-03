e93a68b6210d03665f326bababdc342ed49c9698
MikeFox303 <167519861+MikeFox303@users.noreply.github.com>
2026-09-04 00:20:43 +0300

    feat: export Moonraker HTTP transport

--- src/foxforge/adapters/moonraker/__init__.py
+++ src/foxforge/adapters/moonraker/__init__.py
@@ -2,6 +2,7 @@
 # Copyright (C) 2026 MikeFox303
 
 from .adapter import MoonrakerAdapter
+from .http_transport import MoonrakerHttpSettings, MoonrakerHttpTransport
 from .mapping import MOONRAKER_EXTERNAL_SLOT_ID, MOONRAKER_EXTERNAL_UNIT_ID
 from .material_system import MoonrakerMaterialSystemCapability
 from .native import MoonrakerNativeDispatchResult, MoonrakerNativePrintRequest, MoonrakerNativeState
@@ -12,6 +13,8 @@ __all__ = [
     "MOONRAKER_EXTERNAL_SLOT_ID",
     "MOONRAKER_EXTERNAL_UNIT_ID",
     "MoonrakerAdapter",
+    "MoonrakerHttpSettings",
+    "MoonrakerHttpTransport",
     "MoonrakerMaterialSystemCapability",
     "MoonrakerNativeDispatchResult",
     "MoonrakerNativePrintRequest",
